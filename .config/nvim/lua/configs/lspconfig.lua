f1aa970f33dac377060c132b0592e3b3e98eedf7
docnite <docnite0530@gmail.com>
2024-11-05 13:35:33 +0300

    Update nvim

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "nixd", "rust_analyzer", "hyprls" }
+local servers = { "html", "cssls", "nixd", "rust_analyzer", "hyprls", "rubocop" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
