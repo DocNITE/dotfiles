7280b2665af1773a57fcef3b2010f48034478234
docnite <docnite0530@gmail.com>
2024-11-20 20:13:00 +0300

    Added addional functions for sh

--- .config/sh/global.sh
+++ .config/sh/global.sh
@@ -1,3 +1,4 @@
+
 # yazi shell wrapper
 function y() {
 	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
@@ -24,4 +25,42 @@ function emopicker9000() {
         printf "$chosen" | wl-copy
 	    notify-send "'$chosen' copied to clipboard." &
     fi
+}
+
+# Build nix(flake) config and switch. Config should be placed in 'home/{user}/.nix-config'
+function flake-rebuild() {
+  local ppath="$(pwd)"
+  
+  # Change to the tools directory
+  if cd ~/.nix-config/tools; then
+    # Run the build script with the hostname
+    if ! sh build.sh "$(hostname)"; then
+      echo "Error: build.sh failed to execute."
+    fi
+  else
+    echo "Error: Failed to change directory to ~/.nix-config/tools."
+  fi
+
+  # Return to the original directory
+  cd "$ppath" || echo "Warning: Could not change back to $ppath."
+}
+
+# Update flake repository
+function flake-update() {
+  local ppath="$(pwd)"
+  
+  # Change to the tools directory
+  if cd ~/.nix-config/tools; then
+    # Run the build script with the hostname
+    if ! sh update.sh; then
+      echo "Error: update.sh failed to execute."
+    else 
+      flake-rebuild
+    fi
+  else
+    echo "Error: Failed to change directory to ~/.nix-config/tools."
+  fi
+
+  # Return to the original directory
+  cd "$ppath" || echo "Warning: Could not change back to $ppath."
 }
